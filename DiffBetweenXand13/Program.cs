47e40b3ea45597be949d4e3e88345e4bb03fe168
magnetoplasmadynamics <link1922@gmail.com>
2020-03-14 15:45:20 -0500

    message

---
+++ DiffBetweenXand13/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiffBetweenXand13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int userInput;
+            Console.WriteLine("Enter a number: ");
+            userInput = Convert.ToInt32(Console.ReadLine());
+
+            if(userInput <= 13)
+            {
+                Console.WriteLine(13 - userInput);
+            }
+            else
+            {
+                Console.WriteLine(2 * Math.Abs(13 - userInput));
+            }
+            Console.ReadLine();
+        }
+    }
+}
